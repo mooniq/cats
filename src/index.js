9eb5becebfd2f924b79f12ad3fc3aa069cbe0156
Mooniq <mooniq.marlie@zando.co.za>
2019-04-14 23:17:35 +0200

    get cat data and style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+        families: ['Raleway:300', 'Roboto Slab']
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
