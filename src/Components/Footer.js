60c555709a0c648de43b4ab1ed07c37c793ab811
Mooniq <mooniq.marlie@zando.co.za>
2019-04-14 23:17:57 +0200

    components

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Footer extends Component {
+    render() {
+            return (
+                <footer className="page-footer bg-dark font-small black pt-4">
+                    <div className="footer text-white text-center py-3">
+                        Page created by
+                        <b> Mooniq Marlie</b>.
+                    </div>
+                </footer>
+            );
+    }
+}
+
+export default Footer;
